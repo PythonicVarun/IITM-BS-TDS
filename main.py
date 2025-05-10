7702ccae0d7473afbf3eacab68b806aafc78d50a
Varun Agnihotri <varunagnihotri2028@gmail.com>
2025-05-10 13:48:04 +0000

    * API completed.

---
+++ main.py
@@ -0,0 +1,45 @@
+import csv
+import json
+from typing import Dict, List
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+mData: List[Dict[str, str | int]] = json.load(open("data/q-vercel-python.json"))
+
+cReader = csv.reader(open("data/q-fastapi.csv"))
+cData: Dict[str, List[Dict[str, str]]] = dict(students=[])
+
+next(cReader)
+for d in cReader:
+    cData["students"].append({"studentId": d[0], "class": d[1]})
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+@app.get("/api")
+async def marksApi(request: Request):
+    if q := request.query_params.getlist("name"):
+        return dict(
+            marks = [d["marks"] for d in filter(lambda d: d["name"] in q, mData)]
+        )
+    if c := request.query_params.getlist("class"):
+        return dict(
+            students = [d for d in filter(lambda d: d["class"] in c, cData["students"])]
+        )
+    return cData
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
